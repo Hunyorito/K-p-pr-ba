d3ca844de9da1f22605061aa815f20618e8dbca8
Hunyorító <fecsohunor@gmail.com>
2026-04-21 21:35:09 +0200

    Vegpontok_müködnek

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -41,17 +41,52 @@ export class UserController {
   }
 
 }*/
-import { Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { BadRequestException, Controller, Delete, Get, Param, ParseIntPipe, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { UserService } from "./user.service";
 
 @Controller("user")
 export class UserController {
   constructor(private readonly uploadService: UserService) {}
-
+  /*
   @Post("upload")
   @UseInterceptors(FileInterceptor("file"))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return this.uploadService.upload(file);
   }
+  /*/
+  @Post("upload")
+  @UseInterceptors(FileInterceptor("file", { limits: { fileSize: 2 * 1024 * 1024 } }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const userId=1
+    if(!file){
+      throw new BadRequestException("No file uploaded")
+    }
+    if(!file.mimetype.startsWith("image/")){
+      throw new BadRequestException("Only pictures are allowed")
+    }
+    return this.uploadService.upload(file,userId);
+  }
+  @Get("images")
+  getImages(){
+    const userId=1
+    return this.uploadService.getImages(userId)
+  }
+  @Get("image-count")
+  getImageCount(){
+    const userId=1
+    return this.uploadService.getImageCount(userId)
+  }
+  @Post("profile/:id")
+  setProfile(@Param("id",ParseIntPipe) id:number){
+    const userId=1
+    return this.uploadService.setProfile(userId,id)
+  }
+  @Delete(":id")
+  deletImag(@Param("id",ParseIntPipe) id:number){
+    const userId=1
+    return this.uploadService.deletImag(id,userId)
+  }
+
+  
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { s3 } from "src/s3/s3.service";
 import { Express } from "express";
+
+import { PrismaService } from 'src/prisma.service';
+
 @Injectable()
 export class UserService {
+  constructor(private readonly db:PrismaService) {}
+  /*
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -25,26 +30,107 @@ export class UserService {
   remove(id: number) {
     return `This action removes a #${id} user`;
   }
+  /*/
   private bucket = "test-image";
+  async getImages(userId:number){
+    console.log("teszt get images")
+    const images=await this.db.image.findMany({
+      where:{
+        userId,
+        deleted: false
+      }
+      
+    })
+    const val=images.map(img=>({
+      id:img.id,
+      IsProfile:img.IsProfile,
+      url:`http://localhost:9000/${this.bucket}/${img.key}`
 
-  async upload(file: Express.Multer.File) {
+    }))
+    console.log(val)
+    return images.map(img=>({
+      id:img.id,
+      IsProfile:img.IsProfile,
+      url:`http://localhost:9000/${this.bucket}/${img.key}`
+
+    }))
+  }
+  async getImageCount(userId:number){
+    const count=await this.db.image.count({
+      where:{
+        userId,
+        deleted: false
+      }
+    })
+    return count
+  }
+  async setProfile(userId:number, id:number){
+    await this.db.image.updateMany({
+      where:{userId},
+      data:{IsProfile:false}
+    });
+    return this.db.image.update({
+      where: {id:id},
+      data:{IsProfile:true},
+    })
+  }
+
+  async upload(file: Express.Multer.File, userId:number) {
+    const Max_Imag=5;
     console.log("Edg 1")
-    const fileName = `${Date.now()}-${file.originalname}`;
+    const count=await this.db.image.count({
+      where:{
+        userId,
+        deleted: false
+      }
+    })
+    if(count>=Max_Imag){
+      throw new BadRequestException("Reached maximum image limit")
+
+    }
+  const safeName = file.originalname.replace(/[^a-zA-Z0-9.\-_]/g, "_");
+  const key = `${Date.now()}-${safeName}`;
+  const url = `http://localhost:9000/${this.bucket}/${key}`;
 
     await s3.send(
       new PutObjectCommand({
         Bucket: this.bucket,
-        Key: fileName,
+        Key: key,
         Body: file.buffer,
         ContentType: file.mimetype,
       })
     );
 
+    const image=await this.db.image.create({
+      data:{
+        userId,
+        key,
+        url,
+        IsProfile: false,
+        
+      }
+    })
+
     return {
       message: "Upload successful",
-      fileName,
-      url: `http://localhost:9000/${this.bucket}/${fileName}`,
+      id:image.id,
+      url: url,
     };
   }
-  
+  async deletImag(id:number,userId:number){
+    
+    return await this.db.image.update({
+      where:{id:id},
+      data:{deleted:true}
+    })
+  }
 }
+  /*
+  async deleteImage(id:number){
+    return await this.db.image.update({
+      where:{id:id},
+      data:{deleted:true }
+    })
+  }
+  /*/
+
